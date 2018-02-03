638c270221ddbb66ea35f7f84236f07f25d34486
David Halls <dahalls@gmail.com>
2018-02-03 22:41:01 +0000

    Add recv_from and send_to

--- src/lora_comms.cc
+++ src/lora_comms.cc
@@ -17,13 +17,15 @@ private:
     static void Stop(const Napi::CallbackInfo& info);
     static void Reset(const Napi::CallbackInfo& info);
 
+    static void RecvFrom(const Napi::CallbackInfo& info);
+    static void SendTo(const Napi::CallbackInfo& info);
+
     static void SetGWSendHWM(const Napi::CallbackInfo& info);
     static void SetGWSendTimeout(const Napi::CallbackInfo& info);
     static void SetGWRecvTimeout(const Napi::CallbackInfo& info);
 
-    static struct timeval *OptionalTimeVal(const Napi::CallbackInfo& info,
-                                           struct timeval *tv,
-                                           const uint32_t arg);
+    static struct timeval TimeVal(const Napi::CallbackInfo& info,
+                                  const uint32_t arg);
 };
 
 LoRaComms::LoRaComms(const Napi::CallbackInfo& info) :
@@ -53,6 +55,17 @@ protected:
 private:
     std::string cfg_dir;
 };
+
+Napi::Error ErrnoError(const Napi::Env& env, const int errnum)
+{
+    char buf[1024] = {0};
+    auto errmsg = strerror_r(errnum, buf, sizeof(buf));
+    static_assert(std::is_same<decltype(errmsg), char*>::value,
+                  "strerror_r must return char*");
+    Napi::Error err = Napi::Error::New(env, errmsg ? errmsg : std::to_string(errnum));
+    err.Set("errno", Napi::Number::New(env, errnum));
+    return err;
+}
 
 void LoRaComms::Start(const Napi::CallbackInfo& info)
 {
@@ -86,38 +99,141 @@ void LoRaComms::Reset(const Napi::CallbackInfo& info)
     reset();
 }
 
+class CommsAsyncWorker : public Napi::AsyncWorker
+{
+public:
+    CommsAsyncWorker(const Napi::Function& callback,
+                     const int link,
+                     const Napi::Buffer<uint8_t>& buffer,
+                     const struct timeval& timeout) :
+        Napi::AsyncWorker(callback),
+        link(link),
+        buf(buffer.Data()),
+        len(buffer.Length()),
+        timeout(timeout),
+        buffer_ref(Napi::Persistent(buffer))
+    {
+    }
+
+protected:
+    virtual ssize_t Communicate() = 0;
+
+    void Execute() override
+    {
+        result = Communicate();
+        if (result < 0)
+        {
+            errnum = errno;
+        }
+    }
+
+    void OnOK() override
+    {
+        Napi::Env env = Env();
+        Callback().MakeCallback(
+            Receiver().Value(),
+            std::initializer_list<napi_value>
+            {
+                result < 0 ? ErrnoError(env, errnum).Value() : env.Null(),
+                Napi::Number::New(env, result)
+            });
+    }
+
+    int link;
+    void *buf;
+    size_t len;
+    struct timeval timeout;
+
+private:
+    Napi::Reference<Napi::Buffer<uint8_t>> buffer_ref;
+    ssize_t result;
+    int errnum;
+};
+
+class RecvFromAsyncWorker : public CommsAsyncWorker
+{
+public:
+    RecvFromAsyncWorker(const Napi::Function& callback,
+                        const int link,
+                        const Napi::Buffer<uint8_t>& buffer,
+                        const struct timeval& timeout) :
+        CommsAsyncWorker(callback, link, buffer, timeout)
+    {
+    }
+
+protected:
+    ssize_t Communicate() override
+    {
+        return recv_from(link, buf, len, &timeout);
+    }
+};
+
+void LoRaComms::RecvFrom(const Napi::CallbackInfo& info)
+{
+    (new RecvFromAsyncWorker(info[4].As<Napi::Function>(),
+                             info[0].As<Napi::Number>(),
+                             info[1].As<Napi::Buffer<uint8_t>>(),
+                             TimeVal(info, 2)))
+        ->Queue();
+}
+
+class SendToAsyncWorker : public CommsAsyncWorker
+{
+public:
+    SendToAsyncWorker(const Napi::Function& callback,
+                      const int link,
+                      const Napi::Buffer<uint8_t>& buffer,
+                      ssize_t hwm,
+                      const struct timeval& timeout) :
+        CommsAsyncWorker(callback, link, buffer, timeout),
+        hwm(hwm)
+    {
+    }
+
+protected:
+    ssize_t Communicate() override
+    {
+        return send_to(link, buf, len, hwm, &timeout);
+    }
+
+private:
+    ssize_t hwm;
+};
+
+void LoRaComms::SendTo(const Napi::CallbackInfo& info)
+{
+    (new SendToAsyncWorker(info[5].As<Napi::Function>(),
+                           info[0].As<Napi::Number>(),
+                           info[1].As<Napi::Buffer<uint8_t>>(),
+                           info[2].As<Napi::Number>(),
+                           TimeVal(info, 3)))
+        ->Queue();
+}
+
 void LoRaComms::SetGWSendHWM(const Napi::CallbackInfo& info)
 {
     set_gw_send_hwm(info[0].As<Napi::Number>(), info[1].As<Napi::Number>());
 }
 
-struct timeval *LoRaComms::OptionalTimeVal(const Napi::CallbackInfo& info,
-                                           struct timeval *ptv,
-                                           const uint32_t arg)
-{
-    if ((info.Length() - arg) < 2)
-    {
-        return nullptr;
-    }
-
-    ptv->tv_sec = info[arg].As<Napi::Number>();
-    ptv->tv_usec = info[arg+1].As<Napi::Number>();
-
-    return ptv;
-}
-
 void LoRaComms::SetGWSendTimeout(const Napi::CallbackInfo& info)
 {
-    struct timeval tv;
-    set_gw_send_timeout(info[0].As<Napi::Number>(),
-                        OptionalTimeVal(info, &tv, 1));
+    struct timeval tv = TimeVal(info, 1);
+    set_gw_send_timeout(info[0].As<Napi::Number>(), &tv);
 }
 
 void LoRaComms::SetGWRecvTimeout(const Napi::CallbackInfo& info)
 {
+    struct timeval tv = TimeVal(info, 1);
+    set_gw_recv_timeout(info[0].As<Napi::Number>(), &tv);
+}
+
+struct timeval LoRaComms::TimeVal(const Napi::CallbackInfo& info,
+                                  const uint32_t arg)
+{
     struct timeval tv;
-    set_gw_recv_timeout(info[0].As<Napi::Number>(),
-                        OptionalTimeVal(info, &tv, 1));
+    tv.tv_sec = info[arg].As<Napi::Number>();
+    tv.tv_usec = info[arg+1].As<Napi::Number>();
+    return tv;
 }
 
 Napi::Object LoRaComms::Initialize(Napi::Env env, Napi::Object exports)
@@ -128,9 +244,18 @@ Napi::Object LoRaComms::Initialize(Napi::Env env, Napi::Object exports)
         StaticMethod("stop", &Stop),
         StaticMethod("reset", &Reset),
 
+        StaticMethod("recv_from", &RecvFrom),
+        StaticMethod("send_to", &SendTo),
+
         StaticMethod("set_gw_send_hwm", &SetGWSendHWM),
         StaticMethod("set_gw_send_timeout", &SetGWSendTimeout),
-        StaticMethod("set_gw_recv_timeout", &SetGWRecvTimeout)
+        StaticMethod("set_gw_recv_timeout", &SetGWRecvTimeout),
+
+        StaticValue("EBADF", Napi::Number::New(env, EBADF)),
+        StaticValue("EAGAIN", Napi::Number::New(env, EAGAIN))
+
+        // TODO:
+        // send and receive max buffer sizes
     }));
 
     return exports;
