916c69f0ea2653146a81d23cfbf0f2f54c5f9d10
David Halls <dahalls@gmail.com>
2018-02-07 21:24:41 +0000

    packet_forwarder_shared 0.0.4

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 set -e
 set -x
 VER_LGS=shared-v0.0.1
-VER_PFS=shared-v0.0.3
+VER_PFS=shared-v0.0.4
 if [ ! -d lora_gateway_shared-$VER_LGS ]; then
     wget -O - https://github.com/davedoesdev/lora_gateway_shared/archive/$VER_LGS.tar.gz | tar -zx
 fi
