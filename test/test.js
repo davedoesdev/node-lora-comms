2218f6517157ad70b9bed244cfe4655b5dc9d248
David Halls <dahalls@gmail.com>
2018-02-26 08:57:49 +0000

    Working test

--- test/test.js
+++ test/test.js
@@ -3,6 +3,7 @@
 const lora_comms = require('..'),
       lora_packet = require('lora-packet'),
       path = require('path'),
+      crypto = require('crypto'),
       aw = require('awaitify-stream'),
       expect = require('chai').expect,
       PROTOCOL_VERSION = 2,
@@ -19,15 +20,32 @@ let uplink, downlink;
 
 async function wait_for(link, pkt)
 {
-    let data;
-    do
+    while (true)
     {
-        data = await link.readAsync();
+        let data = await link.readAsync();
+        if ((data.length >= 4) && (data[0] === PROTOCOL_VERSION))
+        {
+            let type = data[3];
+
+            if (type === pkts.PUSH_DATA)
+            {
+                data[3] = pkts.PUSH_ACK;
+                await uplink.writeAsync(data.slice(0, 4));
+            }
+
+            if (type === pkts.PULL_DATA)
+            {
+                data[3] = pkts.PULL_ACK;
+                await downlink.writeAsync(data.slice(0, 4));
+            }
+
+            if (type === pkt)
+            {
+                return data;
+            }
+        }
+
     }
-    while ((data.length < 4) ||
-           (data[0] !== PROTOCOL_VERSION) ||
-           (data[3] !== pkt));
-    return data;
 }
 
 before(function ()
@@ -81,27 +99,27 @@ describe('echoing device', function ()
     {
         this.timeout(60 * 60 * 1000);
 
+        await wait_for(downlink, pkts.PULL_DATA);
+
+        let send_payload = crypto.randomBytes(12);
+        let count = 0;
+
         while (true)
         {
-            let packet = await wait_for(downlink, pkts.PULL_DATA);
-            packet[3] = pkts.PULL_ACK;
-            await downlink.writeAsync(packet.slice(0, 4));
-
-            packet = await wait_for(uplink, pkts.PUSH_DATA);
-            packet[3] = pkts.PUSH_ACK;
-            await uplink.writeAsync(packet.slice(0, 4));
-
+            let packet = await wait_for(uplink, pkts.PUSH_DATA);
             let payload = JSON.parse(packet.slice(12));
-            if (!payload.rxpk)
+            if (!payload.rxpk || !payload.rxpk[0])
             {
                 continue;
             }
+            let rxpk = payload.rxpk[0];
 
-            let data = Buffer.from(payload.rxpk[0].data, 'base64');
-            let decoded = lora_packet.fromWire(data);
+            let recv_data = Buffer.from(rxpk.data, 'base64');
+            let decoded = lora_packet.fromWire(recv_data);
             let buffers = decoded.getBuffers();
 
-            if (!buffers.DevAddr.equals(Buffer.alloc(4)))
+            let DevAddr = Buffer.alloc(4); // TODO: Use non-zero keys
+            if (!buffers.DevAddr.equals(DevAddr))
             {
                 continue;
             }
@@ -109,10 +127,81 @@ describe('echoing device', function ()
             let NwkSKey = Buffer.alloc(16); // TODO: Use non-zero keys
             expect(lora_packet.verifyMIC(decoded, NwkSKey)).to.be.true;
 
-            let AppSKey = Buffer.alloc(16);
-            console.log(lora_packet.decrypt(decoded, AppSKey, NwkSKey));
+            let AppSKey = Buffer.alloc(16); // TODO: Use non-zero keys
+            let recv_payload = lora_packet.decrypt(decoded, AppSKey, NwkSKey);
 
-            break;
+            if (recv_payload.compare(send_payload, 6, send_payload.length, 6) === 0)
+            {
+                break;
+            }
+
+            send_payload = Buffer.concat([recv_payload.slice(0, 6),
+                                          crypto.randomBytes(6)]);
+
+            let encoded = lora_packet.fromFields({
+                MType: 'Unconfirmed Data Down',
+                DevAddr: DevAddr,
+                FCnt: count++,
+                FCtrl: {
+                    ADR: false,
+                    ACK: false,
+                    ADRACKReq: false,
+                    FPending: false
+                },
+                FPort: 1,
+                payload: send_payload
+            }, AppSKey, NwkSKey);
+
+            let send_data = encoded.getPHYPayload();
+
+            let header = Buffer.alloc(4);
+            header[0] = PROTOCOL_VERSION;
+            crypto.randomFillSync(header, 1, 2);
+            header[3] = pkts.PULL_RESP;
+
+            let txpk = {
+                imme: true,
+                freq: rxpk.freq,
+                rfch: 0, // only 0 can transmit
+                powe: 14,
+                modu: rxpk.modu,
+                datr: rxpk.datr,
+                codr: rxpk.codr,
+                ipol: false,
+                prea: 8,
+                size: send_data.length,
+                data: send_data.toString('base64')
+            };
+
+            let databuf = Buffer.concat([header, Buffer.from(JSON.stringify({txpk: txpk}))]);
+            await downlink.writeAsync(databuf);
+
+            let tx_ack = await wait_for(downlink, pkts.TX_ACK);
+            expect(tx_ack[1]).to.equal(header[1]);
+            expect(tx_ack[2]).to.equal(header[2]);
+
+/*
+{ rxpk: 
+   [ { tmst: 54497595,
+       chan: 1,
+       rfch: 1,
+       freq: 868.3,
+       stat: 1,
+       modu: 'LORA',
+       datr: 'SF7BW125',
+       codr: '4/5',
+       lsnr: 10.2,
+       rssi: -5,
+       size: 25,
+       data: 'QAAAAAAAPgABEUCVTKXXJqtyU18zz04VpQ==' } ] }
+*/
+
+
+
+            // make packet for forwarder
+
+
+            // send
         }
 
 
