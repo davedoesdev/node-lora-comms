bee5edefa4c3bba67b3a83e790ba68b1a1e16b62
David Halls <dahalls@gmail.com>
2020-08-26 23:14:23 +0100

    Revise coverage commands

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,20 +9,24 @@ module.exports = function (grunt)
                 cmd: 'node-gyp build --debug'
             },
 
+            rebuild: {
+                cmd: 'node-gyp rebuild --debug'
+            },
+
             cover_build: {
                 cmd: 'node-gyp rebuild --debug --coverage=true'
             },
 
             cover_init: {
-                cmd: 'lcov --rc lcov_branch_coverage=0 --zerocounters --directory build'
+                cmd: 'mkdir -p coverage && rm -f coverage/lcov_addon_base.info && lcov --rc lcov_branch_coverage=0 --zerocounters --directory build && lcov --rc lcov_branch_coverage=0 --capture --initial --directory build --output-file coverage/lcov_addon_base.info'
             },
 
             cover: {
-                cmd: "./node_modules/.bin/nyc -x Gruntfile.js -x 'test/**' node ./node_modules/.bin/grunt test"
+                cmd: "./node_modules/.bin/nyc -x Gruntfile.js -x 'test/**' ./node_modules/.bin/grunt test"
             },
 
             cover_lcov: {
-                cmd: "rm -f coverage/lcov.info && ./node_modules/.bin/nyc report -r lcovonly && rm -f coverage/lcov_addon.info && lcov --rc lcov_branch_coverage=0 --capture --directory build --output-file coverage/lcov_addon.info && rm -f coverage/lcov_addon2.info && lcov --rc lcov_branch_coverage=0 --remove coverage/lcov_addon.info '/usr/*' $PWD'/node_modules/*' --output-file coverage/lcov_addon2.info && rm -f coverage/lcov2.info && lcov --rc lcov_branch_coverage=1 --add-tracefile coverage/lcov.info --add-tracefile coverage/lcov_addon2.info --output-file coverage/lcov2.info"
+                cmd: "rm -f coverage/lcov.info && ./node_modules/.bin/nyc report -r lcovonly && rm -f coverage/lcov_addon.info && lcov --rc lcov_branch_coverage=0 --capture --directory build --output-file coverage/lcov_addon.info && rm -f coverage/lcov_combined.info && lcov --rc lcov_branch_coverage=1 --add-tracefile coverage/lcov.info --add-tracefile coverage/lcov_addon_base.info --add-tracefile coverage/lcov_addon.info --output-file coverage/lcov_combined.info && rm -f coverage/lcov_final.info && lcov --rc lcov_branch_coverage=1 --remove coverage/lcov_combined.info '/usr/*' $PWD'/node_modules/*' --output-file coverage/lcov_final.info"
             },
 
             cover_report: {
@@ -31,11 +35,11 @@ module.exports = function (grunt)
 
             cover_check: {
                 // lines% functions% branches%
-                cmd: "if [ \"$(lcov --rc lcov_branch_coverage=1 --list coverage/lcov2.info | grep Total | grep -o '[0-9.]\\+%' | tr '\\n' ' ')\" != '100% 100% 100% ' ]; then exit 1; fi"
+                cmd: "if [ \"$(lcov --rc lcov_branch_coverage=1 --list coverage/lcov_final.info | grep Total | grep -o '[0-9.]\\+%' | tr '\\n' ' ')\" != '100% 100% 100% ' ]; then exit 1; fi"
             },
 
             coveralls: {
-                cmd: 'cat coverage/lcov2.info | ./node_modules/.bin/coveralls'
+                cmd: 'cat coverage/lcov_final.info | ./node_modules/.bin/coveralls'
             },
 
             documentation: {
@@ -69,6 +73,7 @@ module.exports = function (grunt)
     grunt.loadNpmTasks('grunt-mocha-test');
 
     grunt.registerTask('build', 'exec:build');
+    grunt.registerTask('rebuild', 'exec:rebuild');
     grunt.registerTask('lint', 'eslint');
     grunt.registerTask('test', 'mochaTest');
     grunt.registerTask('coverage', ['exec:cover_build',
